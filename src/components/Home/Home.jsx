5016ee3bc9e0c6d742537817e7a5a57d6b6d9cd2
Pablo Andreu <99558817+pandma@users.noreply.github.com>
2022-07-02 14:24:03 +0200

    key frames animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
         <>
             <section id="section1">
                 <Container fluid>
-                    <Row>
+                    <Row className="animate">
                         <Col id="title" lg={{ span: 6 }} md={{ span: 12 }} style={{ padding: "0px 60px" }}>
                             <h1>¿Cuanto Podemos bajar en tu factura de la luz?</h1>
 
@@ -31,25 +31,25 @@ const Home = () => {
                     <h2>¿A que nos dedicamos?</h2>
                     <h4 id="t-h4">Ayudamos a empresas a ahorrar en su factura de la luz.</h4>
                     <Row>
-                        <Col lg={{ span: 6 }} style={{ padding: "0px" }}>
+                        <Col className="animateImg glow delay-1" lg={{ span: 6 }} style={{ padding: "0px" }}>
                             <Image
                                 id="im"
                                 src="https://res.cloudinary.com/dzzkeb6xp/image/upload/v1656244479/istockphoto-1185621229-612x612_jn7l5p.jpg"
                             />
                         </Col>
-                        <Col lg={{ span: 6 }} style={{ padding: "0px" }}>
+                        <Col className="animateImg glow delay-2" lg={{ span: 6 }} style={{ padding: "0px" }}>
                             <Image
                                 id="im"
                                 src="https://res.cloudinary.com/dzzkeb6xp/image/upload/v1656244479/istockphoto-1164863397-612x612_w8ptfa.jpg"
                             />
                         </Col>
-                        <Col lg={{ span: 6 }} style={{ padding: "0px" }}>
+                        <Col className="animateImg glow delay-3" lg={{ span: 6 }} style={{ padding: "0px" }}>
                             <Image
                                 id="im"
                                 src="https://res.cloudinary.com/dzzkeb6xp/image/upload/v1656244479/istockphoto-1178152960-612x612_tifmzf.jpg"
                             />
                         </Col>
-                        <Col id="sect3text" lg={{ span: 6 }} style={{ padding: "0px" }}>
+                        <Col className="animateImg glow delay-4" id="sect3text" lg={{ span: 6 }} style={{ padding: "0px" }}>
                             <h3>Contactamos con Fiva porque nuestra factura habia aumentado un 30% desde 2020 y
                                 gracias a Fiva hemos ahorrado 2000€ en el ultimo año.</h3>
                             <p>Julian</p>
